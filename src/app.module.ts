0b28f959e644d4f149ea2ead5a76435692dfbf9d
Max <max.poblete2905@gmail.com>
2025-04-12 22:35:27 -0400

    delete interceptor

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,7 +3,6 @@ import { FirebaseModule } from './firebase/firebase.module';
 import { ProjectsModule } from './projects/projects.module';
 import { CacheModule } from '@nestjs/cache-manager';
 import { APP_INTERCEPTOR } from '@nestjs/core';
-import { CacheKeyInterceptor } from './Interceptors/cacheKey.Interceptor';
 
 @Module({
   imports: [
@@ -13,12 +12,6 @@ import { CacheKeyInterceptor } from './Interceptors/cacheKey.Interceptor';
       ttl: 30, // Tiempo de vida en segundos (30s)
       max: 100, // Máximo número de items en caché
     }),
-  ],
-  providers: [
-    {
-      provide: APP_INTERCEPTOR,
-      useClass: CacheKeyInterceptor,
-    },
   ],
 })
 export class AppModule { }
